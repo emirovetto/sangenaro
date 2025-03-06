761d8522c282e2b99365651d9514d60588d9677c
emirovetto <101435972+emirovetto@users.noreply.github.com>
2025-03-06 16:55:15 -0300

    eslint: {       ignoreDuringBuilds: true, // 🚀 Ignora ESLint en el build

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+      ignoreDuringBuilds: true, // 🚀 Ignora ESLint en el build
+    },
+  };
 
 export default nextConfig;
